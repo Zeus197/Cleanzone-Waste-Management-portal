55187629a5bca9d3510f26bce3581805f8b54018
ishan tickoo <ishantickoo@ishans-mbp.home>
2020-03-26 15:05:09 -0400

    updated files

---
+++ php/feedback1.php
@@ -0,0 +1,473 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+
+<!-- Mirrored from webdesign-finder.com/html/gogreen/contact2.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 09 May 2018 04:44:34 GMT -->
+<head>
+	<title>Waste Management</title>
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/main.css" class="color-switcher-link">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+	<!--[if lt IE 9]>
+		<div class="bg-danger text-center">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="highlight">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
+
+	<div class="preloader">
+		<div class="preloader_image"></div>
+	</div>
+
+	<!-- search modal -->
+	<div class="modal" tabindex="-1" role="dialog" aria-labelledby="search_modal" id="search_modal">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">
+				<i class="rt-icon2-cross2"></i>
+			</span>
+		</button>
+		<div class="widget widget_search">
+			<form method="get" class="searchform search-form form-inline" action="http://webdesign-finder.com/html/gogreen/">
+				<div class="form-group">
+					<input type="text" value="" name="search" class="form-control" placeholder="Search keyword" id="modal-search-input">
+				</div>
+				<button type="submit" class="theme_button">Search</button>
+			</form>
+		</div>
+	</div>
+
+	<!-- Unyson messages modal -->
+	<div class="modal fade" tabindex="-1" role="dialog" id="messages_modal">
+		<div class="fw-messages-wrap ls with_padding">
+			<!-- Uncomment this UL with LI to show messages in modal popup to your user: -->
+			<!--
+		<ul class="list-unstyled">
+			<li>Message To User</li>
+		</ul>
+		-->
+
+		</div>
+	</div>
+	<!-- eof .modal -->
+
+	<!-- wrappers for visual page editor and boxed version of template -->
+	<div id="canvas">
+		<div id="box_wrapper">
+
+			<!-- template sections -->
+
+			<section class="page_topline cs two_color section_padding_top_5 section_padding_bottom_5 table_section">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-6 text-center text-sm-left">
+							<p class="divided-content">
+					<span class="medium black">
+						Welcome to Waste Management 
+					</span>
+					<a href="#">How to Find Us</a>
+					<a href="#">Give Feedback</a>
+				</p>
+						</div>
+						<div class="col-sm-6 text-center text-sm-right">
+
+							<div class="widget widget_search">
+								<form method="get" class="searchform form-inline" action="http://webdesign-finder.com/html/gogreen/">
+									<div class="form-group-wrap">
+										<div class="form-group margin_0">
+											<label class="sr-only" for="topline-search">Search for:</label>
+											<input id="topline-search" type="text" value="" name="search" class="form-control" placeholder="Search Keyword">
+										</div>
+										<button type="submit" class="theme_button">Search</button>
+									</div>
+								</form>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<header class="page_header header_white toggler_xs_right section_padding_20">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12 display_table">
+							<div class="header_left_logo display_table_cell">
+								<a href="index.html" class="logo top_logo">
+									<img src="images/logo.png" alt="">
+									<span class="logo_text">
+										<span class="big">Waste Management</span>
+										<span class="small-text">Recycling</span>
+									</span>
+								</a>
+							</div>
+
+							<div class="header_mainmenu display_table_cell text-center">
+								<!-- main nav start -->
+								<nav class="mainmenu_wrapper">
+									<ul class="mainmenu nav sf-menu">
+										<li class="active">
+											<a href="index.php">Home</a>
+											<ul>
+												
+												<li>
+													<a href="main-page.php">Admin Dashboard</a>
+												</li>
+											</ul>
+										</li>
+<li>
+											<a href="about.html">About</a>
+											
+										</li>
+										<!-- eof pages -->
+
+										<li>
+											<a href="#">Feedback</a>
+											
+												
+											
+											<!-- eof mega menu -->
+										</li>
+	
+
+
+										<!-- gallery -->
+										<li>
+											<a href="gallery-regular.html">Gallery</a>
+											<ul>
+												
+												
+												
+
+												<!-- Gallery carousel -->
+												<li>
+													<a href="gallery-carousel.php">Gallery Carousel</a>
+													
+												</li>
+												<!-- eof Gallery carousel -->
+
+											
+											</ul>
+										</li>
+										<li>
+											<a href="blog-right.html">Services</a>
+											<ul>
+
+												<li>
+													<a href="blog-right.html">Dry</a>
+													<ul>
+														<li>
+															<a href="dry_form.php">Dry Wastage</a>
+														</li></ul>
+												</li>
+												<li>
+													<a href="blog-left.html">Wet</a>
+													<ul>
+														<li>
+															<a href="wet_form.php">Wet Wastage</a>
+														</li></ul>
+												</li>
+												<li>
+													<a href="blog-full.html">Recycle System</a>
+													
+												       <ul>
+														<li>
+															<a href="service_dry.php">Recycle Dry Waste</a>
+														</li>
+														
+														<li>
+															<a href="service_wet.php">Recycle Wet Waste</a>
+														</li></ul>
+												</li>
+												
+											</ul>
+										</li>
+										<!-- eof blog -->
+
+										<!-- contacts -->
+										<li>
+											<a href="contact.html">Contact</a>
+											<ul>
+												<li>
+													<a href="contact.html">Contact 1</a>
+												</li>
+												<li>
+													<a href="contact2.html">Contact 2</a>
+												</li>
+												<li>
+													<a href="contact3.html">Contact 3</a>
+												</li>
+												<li>
+													<a href="contact4.html">Contact 4</a>
+												</li>
+											</ul>
+										</li>
+										<!-- eof contacts -->
+									</ul>
+										
+								</nav>
+								<!-- eof main nav -->
+								<!-- header toggler -->
+								<span class="toggle_menu">
+									<span></span>
+								</span>
+							</div>
+
+							<div class="header_right_buttons display_table_cell text-right hidden-xs">
+								<div class="darklinks">
+									<a href="#" class="social-icon border-icon rounded-icon soc-facebook"></a>
+									<a href="#" class="social-icon border-icon rounded-icon soc-twitter"></a>
+									<a href="#" class="social-icon border-icon rounded-icon soc-google"></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<section class="page_breadcrumbs ds parallax section_padding_top_65 section_padding_bottom_65">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<h2 class="highlight">Feedback Form</h2>
+							<ol class="breadcrumb darklinks">
+								<li>
+									<a href="index.html">
+							Home
+						</a>
+								</li>
+								<li>
+									<a href="#">Feedback</a>
+								</li>
+								<li class="active">Feedback Form</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+					<div class="row topmargin_40">
+						<div class="col-sm-12 to_animate">
+							<form class="" method="post" action="feedback1db.php">
+								<div class="row">
+
+									<div class="col-sm-6">
+										<p class="form-group">
+                                <label for="name">Full Name <span class="required">*</span></label>
+                                
+                                <input type="text" aria-required="true" size="30" value="" name="name" id="name" class="form-control" placeholder="Full Name">
+                            </p>
+										<p class="form-group">
+                                <label for="email">Email address<span class="required">*</span></label>
+                                <i class="fa fa-envelope highlight2" aria-hidden="true"></i>
+                                <input type="email" aria-required="true" size="30" value="" name="email" id="email" class="form-control" placeholder="Email Address">
+                            </p>
+										<p class="form-group">
+                                <label for="subject">Subject<span class="required">*</span></label>
+                                <i class="fa fa-flag highlight2" aria-hidden="true"></i>
+                                <input type="text" aria-required="true" size="30" value="" name="subject" id="subject" class="form-control" placeholder="Subject">
+                            </p>
+									</div>
+									<div class="col-sm-6">
+
+										<p class="contact-form-message form-group">
+                                <label for="feedback">Your Feedback</label>
+                                <i class="fa fa-comment-o highlight2" aria-hidden="true"></i>
+                                <textarea aria-required="true" rows="5" cols="45" name="feedback" id="feedback" class="form-control" placeholder="Your Feedback"></textarea>
+                            </p>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-sm-12">
+
+										<p class="">
+                                <input type="submit"  name="FEED"  VALUE="feed"class="theme_button color1" >Send Feedback</submit>
+                            </p>
+									</div>
+
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<?php include("footer.php");
+?>
+			
+			<!--<footer class="page_footer ds section_padding_top_100 section_padding_bottom_65 columns_padding_25">
+				<div class="container">
+					<div class="row">
+
+						<div class="col-md-4 col-sm-12 text-center">
+							<div class="widget">
+								<a href="index.html" class="logo bg_logo bottommargin_25">
+									<img src="images/logo.png" alt="">
+									<span class="logo_text">
+										<span class="big">GoGreen</span>
+										<span class="small-text">Recycling</span>
+									</span>
+								</a>
+								<p>
+						Family-owned company from San Diego proudly serving individuals and businesses in Southern California.
+					</p>
+								<div class="line-height-thin">
+									<div class="media small-teaser inline-block margin_0">
+										<div class="media-left media-middle">
+											<i class="fa fa-map-marker highlight2" aria-hidden="true"></i>
+										</div>
+										<div class="media-body media-middle grey">
+											140 Horizon Circle, San Diego, CA
+										</div>
+									</div>
+									<br>
+									<div class="media small-teaser inline-block margin_0">
+										<div class="media-left media-middle">
+											<i class="fa fa-phone highlight2" aria-hidden="true"></i>
+										</div>
+										<div class="media-body media-middle grey">
+											01 123 456 789
+										</div>
+									</div>
+									<br>
+									<div class="media small-teaser inline-block margin_0">
+										<div class="media-left media-middle">
+											<i class="fa fa-map-marker highlight2" aria-hidden="true"></i>
+										</div>
+										<div class="media-body media-middle highlightlinks">
+											<a href="#">gogreen@support.com</a>
+										</div>
+									</div>
+								</div>
+
+								<div class="darklinks topmargin_20">
+									<a href="#" class="social-icon border-icon rounded-icon soc-facebook"></a>
+									<a href="#" class="social-icon border-icon rounded-icon soc-twitter"></a>
+									<a href="#" class="social-icon border-icon rounded-icon soc-google"></a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 text-center">
+							<div class="widget widget_contact topmargin_10">
+								<h3>Contact Form</h3>
+								<form class="contact-form topmargin_45" method="post" action="http://webdesign-finder.com/html/gogreen/">
+									<p class="contact-form-name">
+							<label for="footer-name">Name <span class="required">*</span></label>
+							<input type="text" aria-required="true" size="30" value="" name="name" id="footer-name" class="form-control text-center" placeholder="Full Name">
+						</p>
+									<p class="contact-form-email">
+							<label for="footer-email">Email <span class="required">*</span></label>
+							<input type="email" aria-required="true" size="30" value="" name="email" id="footer-email" class="form-control text-center" placeholder="Email Address">
+						</p>
+									<p class="contact-form-message">
+							<label for="footer-message">Message</label>
+							<textarea aria-required="true" rows="3" cols="45" name="message" id="footer-message" class="form-control text-center" placeholder="Message"></textarea>
+						</p>
+									<p class="footer_contact-form-submit topmargin_30">
+							<button type="submit" id="footer_contact_form_submit" name="contact_submit" class="theme_button color1 wide_button">Send Message</button>
+						</p>
+								</form>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 text-center">
+
+							<div class="widget widget_recent_entries topmargin_10">
+
+								<h3 class="widget-title">Recent News</h3>
+								<ul class="greylinks">
+									<li>
+										<p class="margin_0">
+								<a href="blog-single-right.html">
+									At vero eos et accusam et justo duo dolores et ea rebum clita kasd
+								</a>
+							</p>
+										<span class="entry-date highlight">
+											<time class="entry-date" datetime="2017-03-13T08:50:40+00:00">
+												March 9, 2017
+											</time>
+										</span>
+									</li>
+									<li>
+										<p class="margin_0">
+								<a href="blog-single-right.html">
+									Gubergren, no sea takimata sanctus est lorem ipsum dolor sit amet
+								</a>
+							</p>
+										<span class="entry-date highlight">
+											<time class="entry-date" datetime="2017-03-13T08:50:40+00:00">
+												March 9, 2017
+											</time>
+										</span>
+									</li>
+									<li>
+										<p class="margin_0">
+								<a href="blog-single-right.html">
+									Lorem ipsum dolor sit amet, consetetur
+								</a>
+							</p>
+										<span class="entry-date highlight">
+											<time class="entry-date" datetime="2017-03-13T08:50:40+00:00">
+												March 9, 2017
+											</time>
+										</span>
+									</li>
+
+								</ul>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</footer>
+
+			<section class="ls page_copyright section_padding_15">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<p class="small-text small-spacing grey">&copy; Copyright 2017 All Rights Reserved</p>
+						</div>
+					</div>
+				</div>
+			</section>-->
+
+		</div>
+		<!-- eof #box_wrapper -->
+	</div>
+	<!-- eof #canvas -->
+
+	<script src="js/compressed.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/switcher.js"></script>
+
+	<!-- Google Map Script -->
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTwYSMRGuTsmfl2z_zZDStYqMlKtrybxo"></script>
+</body>
+
+
+<!-- Mirrored from webdesign-finder.com/html/gogreen/contact2.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 09 May 2018 04:44:34 GMT -->
+</html>
